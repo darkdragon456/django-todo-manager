09ce448ca37ff7f125b15ad36053a2702d81a2fd
Dragon <beheraritesh699@gmail.com>
2026-06-28 16:40:23 +0530

    configure project for Railway deployment

--- todomanager/settings.py
+++ todomanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ak9(srt!lh0$w3dee(b09wu$lef6jx)o5i@9nq5ycg2tcb1fb^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
