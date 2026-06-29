5ea3171994105af77a29f9fc5f85966d97e9bcfb
Dragon <beheraritesh699@gmail.com>
2026-06-29 23:01:25 +0530

    Backup before responsive edit page

--- todomanager/settings.py
+++ todomanager/settings.py
@@ -122,10 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
